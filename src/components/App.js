1cbca7bc9b75ea5cfdb2ea67b252ec7743f45e88
Adam <50333588+adamcpenman@users.noreply.github.com>
2019-10-15 19:41:10 -0500

    components
    set up basic components

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
+import Header from "./Header";
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <h1>Welcome to Lambda School</h1>
     </div>
   );
